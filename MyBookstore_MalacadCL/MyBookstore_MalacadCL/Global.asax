842a4a955a10270f62aba18956504c4ad73022ab
11604525 <11604525@TAFT-CL312>
2017-09-15 14:03:14 +0800

    Adding NewScrpts creating a successful webpage of lists and create

---
+++ MyBookstore_MalacadCL/MyBookstore_MalacadCL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyBookstore_MalacadCL.MvcApplication" Language="C#" %>
---
+++ MyBookstore_MalacadCL/MyBookstore_MalacadCL/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBookstore_MalacadCL.Startup))]
+namespace MyBookstore_MalacadCL
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
